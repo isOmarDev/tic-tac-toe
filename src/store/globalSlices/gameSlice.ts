a0e7c692330bf59d359cff4a7a65ea3dbf577fab
omar kamel <isomardev@gmail.com>
2024-10-16 16:02:43 +0300

    add state

--- src/store/globalSlices/gameSlice.ts
+++ src/store/globalSlices/gameSlice.ts
@@ -1,12 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '@/store';
 
-interface gameState {
+interface GameState {
   isStarted: boolean;
+  isPlaying: boolean;
 }
 
-const initialState: gameState = {
+const initialState: GameState = {
   isStarted: false,
+  isPlaying: false,
 };
 
 export const gameSlice = createSlice({
@@ -19,12 +21,22 @@ export const gameSlice = createSlice({
     exitGameSetup: (state) => {
       state.isStarted = false;
     },
+    playGame: (state) => {
+      state.isPlaying = true;
+    },
+    stopGame: (state) => {
+      state.isPlaying = false;
+    },
   },
 });
 
-export const { startGameSetup, exitGameSetup } =
-  gameSlice.actions;
+export const {
+  startGameSetup,
+  exitGameSetup,
+  playGame,
+  stopGame,
+} = gameSlice.actions;
 
-export const selectGame = (state: RootState) => state.game;
+export const selectGameState = (state: RootState) => state.game;
 
 export default gameSlice.reducer;
