fd596c88f64971d080b1849b5ac68e197c86deb7
omar kamel <isomardev@gmail.com>
2024-10-16 15:56:52 +0300

    add input error handling and onSuccess cb

--- src/features/players/components/AddPlayerForm.tsx
+++ src/features/players/components/AddPlayerForm.tsx
@@ -1,41 +1,61 @@
-import { useAddPlayerForm } from './useAddPlayerForm';
+import { UserRound } from 'lucide-react';
+import { useAddPlayerForm } from '../hooks/useAddPlayerForm';
+import { InputError } from '@/components/errors';
+import { cn } from '@/utils';
 
 type AddPlayerProps = {
   label: string;
   initialName?: string;
-  onAddPlayer: (name: string) => void;
+  onSuccess?: (name: string) => void;
 };
 
 export const AddPlayerForm = ({
   label,
   initialName = '',
-  onAddPlayer,
+  onSuccess,
 }: AddPlayerProps) => {
-  const { name, handleChange, handleSubmit } = useAddPlayerForm({
-    initialName,
-    onAddPlayer,
-  });
+  const { name, error, handleChange, handleSubmit } =
+    useAddPlayerForm({
+      initialName,
+      onSuccess,
+    });
 
   return (
     <form
+      id="add-player-form"
       className="flex flex-col gap-2"
       onSubmit={handleSubmit}
     >
       <label
-        className="pl-2 font-ubuntu text-xl font-medium tracking-wide text-white"
-        htmlFor="player"
+        className={cn(
+          'flex items-center gap-2 pl-4',
+          'font-ubuntu text-lg font-medium tracking-wide text-white',
+        )}
+        htmlFor="player-input"
       >
-        {label}
+        <UserRound strokeWidth={1} />
+        <span>{label}</span>
       </label>
 
       <input
-        id="player"
+        id="player-input"
         className="rounded-full border-[3px] border-white px-4 py-5 text-white"
         type="text"
         name="player"
         value={name}
+        autoFocus
         onChange={handleChange}
       />
+
+      {error && (
+        <InputError
+          className={cn(
+            'absolute left-1/2 top-[calc(100%+10px)] w-max',
+            '-translate-x-1/2 rounded-full',
+          )}
+          message={error}
+        />
+      )}
     </form>
   );
 };
