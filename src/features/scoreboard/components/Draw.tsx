018d6e3bc8a578bf5d7f5858fecbd6077cd30364
omar kamel <isomardev@gmail.com>
2024-11-17 22:47:45 +0200

    consume draw state from redux

--- src/features/scoreboard/components/Draw.tsx
+++ src/features/scoreboard/components/Draw.tsx
@@ -1,16 +1,20 @@
+import { useSelector } from '@/store';
+import { selectDrawScore } from '../slices';
 import { cn } from '@/utils';
 
 export const Draw = () => {
+  const drawScore = useSelector(selectDrawScore);
+
   return (
     <div
       className={cn(
         'mt-5 px-1 py-2',
         'rounded-full',
-        'text-center text-xs text-white shadow',
-        'bg-cyan-700',
+        'text-center text-xs tracking-widest text-white shadow',
+        'bg-cyan-600',
       )}
     >
-      <span className="font-semibold">Draw:</span> 0
+      <span>Draw:</span> {drawScore}
     </div>
   );
 };
