7010bbbaa58cdaae56ccbc35cddad1613b89c28f
omar kamel <isomardev@gmail.com>
2024-11-17 22:42:26 +0200

    add transition and refactore code

--- src/features/gameboard/components/Gameboard.tsx
+++ src/features/gameboard/components/Gameboard.tsx
@@ -1,21 +1,23 @@
+import { Transition } from '@/components/animation';
 import { Borders } from './Borders';
 import { Cells } from './Cells';
 import { cn } from '@/utils';
 
 export const Gameboard = () => {
   return (
-    <div
-      className={cn(
-        'relative',
-        'mx-auto',
-        'h-[370px] w-[370px]',
-        'rounded-3xl outline outline-[6px] -outline-offset-1 outline-white',
-        'bg-red-500',
-        'overflow-hidden shadow-2xl',
-      )}
-    >
-      <Borders />
-      <Cells />
-    </div>
+    <Transition translateY={100} duration={400}>
+      <div
+        className={cn(
+          'relative overflow-hidden',
+          'mx-auto',
+          'h-[380px] w-[380px]',
+          'rounded-3xl outline outline-[6px] -outline-offset-1 outline-white',
+          'bg-rose-500 shadow-2xl',
+        )}
+      >
+        <Borders />
+        <Cells />
+      </div>
+    </Transition>
   );
 };
