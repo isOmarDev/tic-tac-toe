6891040b1dc5fcdcdd5a8ccbb84065f7b79a318d
omar kamel <isomardev@gmail.com>
2024-11-17 22:52:28 +0200

    delete file

--- src/components/layouts/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export const Footer = () => {
-  return <footer className="mt-20"></footer>;
-};
--- src/components/layouts/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from './Header';
-export { MainContent } from './MainContent';
-export { MainLayout } from './MainLayout';
-export { Sidebar } from './Sidebar';
