cfd89d6babf3a897d22c68b3d9759e2abd2b28c7
omar kamel <isomardev@gmail.com>
2024-11-17 22:31:48 +0200

    create not found page

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,30 +1,42 @@
-import { MoveLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { Transition } from '@/components/animation';
 import { cn } from '@/utils';
 
 export const NotFound = () => {
   return (
-    <div className="mt-52 flex flex-col items-center font-semibold">
-      <h1 className="w-min text-[90px] font-black leading-[0.9]">
-        <span className="text-[50px] font-semibold">
-          Not
-        </span>{' '}
-        Found
-      </h1>
+    <div
+      className={cn(
+        'flex',
+        'flex-col items-center justify-center',
+        'h-screen',
+        'bg-red-50',
+      )}
+    >
+      <Transition translateX={-20} duration={300} delay={50}>
+        <h1 className="-rotate-12 text-[60px] font-semibold leading-[0.9]">
+          page not found
+        </h1>
+      </Transition>
 
-      <Link
-        className={cn(
-          'mt-6 flex w-[276px] gap-2 rounded-full px-4 py-4',
-          'bg-rose-400 text-left text-sm font-medium text-white',
-          'transition-colors duration-150 ease-linear',
-          'hover:bg-rose-300',
-        )}
-        to="/"
-        replace
-      >
-        <MoveLeft size={20} />
-        <span>Go Back</span>
-      </Link>
+      <Transition translateY={16} duration={300} delay={50}>
+        <Link
+          className={cn(
+            'flex',
+            'items-center justify-center gap-2',
+            'mt-16 px-4 py-5',
+            'w-32',
+            'rounded-full',
+            'text-left font-medium text-white',
+            'bg-rose-500',
+            'transition-colors duration-150 ease-linear',
+            'hover:bg-rose-600',
+          )}
+          to="/"
+          replace
+        >
+          Go Back
+        </Link>
+      </Transition>
     </div>
   );
 };
