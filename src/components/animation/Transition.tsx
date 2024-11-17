9cfa4902d91f18fc75c1f2cbea9ec1cd10978d24
omar kamel <isomardev@gmail.com>
2024-11-17 22:34:24 +0200

    pass delay to mount hook

--- src/components/animation/Transition.tsx
+++ src/components/animation/Transition.tsx
@@ -12,37 +12,39 @@ type TransitionProps = {
   children: React.ReactNode;
 };
 
-export const  Transition = ({
-  className,
-  translateX = 0,
-  translateY = 0,
-  scale = 1,
-  duration = 150,
-  delay = 0,
-  children,
-}: TransitionProps) => {
-  const { isMounted } = useIsMounted();
+export const Transition = React.memo(
+  ({
+    className,
+    translateX = 0,
+    translateY = 0,
+    scale = 1,
+    duration = 150,
+    delay = 0,
+    children,
+  }: TransitionProps) => {
+    const { isMounted } = useIsMounted(delay);
 
-  const opacityValue = isMounted ? 1 : 0;
-  const translateValue = isMounted
-    ? '0px, 0px'
-    : `${translateX}px, ${translateY}px`;
+    const opacityValue = isMounted ? 1 : 0;
+    const translateValue = isMounted
+      ? '0px, 0px'
+      : `${translateX}px, ${translateY}px`;
 
-  const scaleValue = isMounted ? '1' : `${scale}`;
+    const scaleValue = isMounted ? '1' : `${scale}`;
 
-  const style: React.CSSProperties = {
-    opacity: opacityValue,
-    transform: `translate(${translateValue}) scale(${scaleValue})`,
-    transitionDuration: `${duration}ms`,
-    transitionDelay: `${delay}ms`,
-  };
+    const style: React.CSSProperties = {
+      opacity: opacityValue,
+      transform: `translate(${translateValue}) scale(${scaleValue})`,
+      transitionDuration: `${duration}ms`,
+      // transitionDelay: `${delay}ms`,
+    };
 
-  return (
-    <div
-      style={style}
-      className={cn(`transition-all`, className)}
-    >
-      {children}
-    </div>
-  );
-};
+    return (
+      <div
+        style={style}
+        className={cn(`transition-all`, className)}
+      >
+        {children}
+      </div>
+    );
+  },
+);
