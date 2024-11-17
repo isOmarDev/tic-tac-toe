ea033be7de5431ec2f0ad8455733943d991e5fab
omar kamel <isomardev@gmail.com>
2024-11-17 22:49:33 +0200

    add transition and refactore code

--- src/features/scoreboard/components/Scoreboard.tsx
+++ src/features/scoreboard/components/Scoreboard.tsx
@@ -1,43 +1,50 @@
 import { Circle, X } from 'lucide-react';
+import { Transition } from '@/components/animation';
 import { PlayerScore } from './PlayerScore';
 import { Round } from './Round';
 import { Draw } from './Draw';
 import { useSelector } from '@/store';
-import { selectPlayersName } from '@/features/players/slices';
+import { selectPlayers } from '@/features/players/slices';
+import { selectPlayersScore } from '../slices';
 
 export const Scoreboard = () => {
-  const { player1, player2 } = useSelector(selectPlayersName);
+  const score = useSelector(selectPlayersScore);
+  const { playerOne, playerTwo } = useSelector(selectPlayers);
 
   return (
-    <div className="flex items-center gap-10">
-      <PlayerScore
-        name={player1}
-        side={
-          <X
-            size={38}
-            strokeWidth={6}
-            className="text-[#ffbf00] drop-shadow-md"
-          />
-        }
-        score="0"
-      />
+    <Transition translateY={-100} duration={400}>
+      <div className="flex items-center gap-10">
+        <PlayerScore
+          name={playerOne.name}
+          symbol={playerOne.symbol}
+          score={score.x}
+          icon={
+            <X
+              className="text-golden-yellow drop-shadow-md"
+              size={40}
+              strokeWidth={6.5}
+            />
+          }
+        />
 
-      <div className="self-start">
-        <Round />
-        <Draw />
+        <div className="self-start">
+          <Round />
+          <Draw />
+        </div>
+
+        <PlayerScore
+          name={playerTwo.name}
+          symbol={playerTwo.symbol}
+          score={score.o}
+          icon={
+            <Circle
+              className="rounded-full drop-shadow-md"
+              size={40}
+              strokeWidth={10}
+            />
+          }
+        />
       </div>
-
-      <PlayerScore
-        name={player2}
-        side={
-          <Circle
-            size={38}
-            strokeWidth={10}
-            className="rounded-full drop-shadow-md"
-          />
-        }
-        score="0"
-      />
-    </div>
+    </Transition>
   );
 };
